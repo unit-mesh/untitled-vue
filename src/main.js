082a9bc1547ec16ad4c4e9d52021afb53824a8db
Phodal Huang <h@phodal.com>
2025-02-17 11:52:50 +0800

    chore: add ElementPlus for UI component integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store' // 导入 store
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(store) // 挂载 store
 app.mount('#app')
